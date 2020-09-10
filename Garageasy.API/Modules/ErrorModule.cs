1dccd5d7f28d9076f80790f71a571baa34f57209
andresorrego94 <andres.orrego.94@gmail.com>
2020-09-09 22:58:53 -0300

    Initial solution

---
+++ Garageasy.API/Modules/ErrorModule.cs
@@ -0,0 +1,36 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garageasy.API.Modules
+{
+    public class ErrorModule: CarterModule
+    {
+        public ErrorModule()
+        {
+            this.Get("/error", (req, res) =>
+            {
+                throw new Exception("oops");
+            });
+
+            this.Get("/errorhandler", (req, res) =>
+            {
+                string error = string.Empty;
+                var feature = req.HttpContext.Features.Get<IExceptionHandlerFeature>();
+                if (feature != null)
+                {
+                    if (feature.Error is ArgumentNullException)
+                    {
+                        res.StatusCode = 402;
+                    }
+                    error = feature.Error.ToString();
+                }
+                return res.WriteAsync($"There has been an error{Environment.NewLine}{error}");
+            });
+        }
+    }
+}
