51f528609c752e186522513e7e6ff5bbe9d00daf
Andres Orrego <andres.orrego.94@iMac-de-Andres.local>
2020-09-28 02:30:44 -0300

    Navigation mitch example with Login basic behavoir, 2 properties

---
+++ AndroidNavigation/app/src/main/java/com/android/example/navigationcomponentsexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.example.navigationcomponentsexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
